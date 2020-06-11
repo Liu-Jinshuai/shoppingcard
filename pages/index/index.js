d4eb96e4f0bd45ccf42bd4f56848344e874ef73c
你敢凝视深渊嘛 <53417969+17562105692@users.noreply.github.com>
2020-06-11 17:59:33 +0800

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data:{
+    alllist:[{
+      name:"hello",
+      price:"100",
+      pic:"../img/demo3.jpg",
+      active:true,
+      number:"1"
+    },
+    {
+      name:"hello2",
+      price:"200",
+      pic:"../img/demo3.jpg",
+      active:true,
+      number:"1"
+    }],
+  }
+})
